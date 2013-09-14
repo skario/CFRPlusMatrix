65c2646251c63a757ef377df23ee6d82e59d2209
skario <ot@iki.fi>
2013-09-14 17:49:39 +0300

    -delay (CFR+ averaging delay)

--- CFRPlusMatrix.cpp
+++ CFRPlusMatrix.cpp
@@ -66,20 +66,20 @@ public:
 		CFR(1);
 	}
 
-	void CFRPlus()
+	void CFRPlus(int delay)
 	{
 		iterationCount++;
-		CFRPlus(0);
-		CFRPlus(1);
+		CFRPlus(0, delay);
+		CFRPlus(1, delay);
 	}
 
-	void Iteration(int algorithm)
+	void Iteration(int algorithm, int delay)
 	{
 		switch (algorithm)
 		{
 		case 0: FictitiousPlay(); break;
 		case 1: CFR(); break;
-		default: CFRPlus(); break;
+		default: CFRPlus(delay); break;
 		}
 	}
 
@@ -233,7 +233,7 @@ private:
 
 	}
 
-	void CFRPlus(int player)
+	void CFRPlus(int player, int delay)
 	{
 		vector<double> cfu(size);
 		double ev = 0;
@@ -254,8 +254,10 @@ private:
 		for (int a = 0; a < size; a++)
 			cfr[player][a] = std::max(0.0, cfr[player][a] + cfu[a] - ev);
 
+		double w = iterationCount > delay ? (iterationCount - delay) * (iterationCount - delay) : 0;
+
 		for (int a = 0; a < size; a++)
-			strategy[player][a] += sp[a] * iterationCount * iterationCount;
+			strategy[player][a] += sp[a] * w;
 
 	}
 
@@ -269,20 +271,20 @@ private:
 
 };
 
-int Run(int algorithm, int size, double epsilon, mt19937 &rng)
+int Run(int algorithm, int delay, int size, double epsilon, mt19937 &rng)
 {
 	MatrixGame m(size, rng);
 	double e;
 	do
 	{
-		m.Iteration(algorithm);
+		m.Iteration(algorithm, delay);
 		e = m.GetExploitability();
 	} while (e > epsilon);
 
 	return m.GetIterationCount();
 }
 
-void RunMany(int n, int algorithm, int size, double epsilon)
+void RunMany(int n, int algorithm, int delay, int size, double epsilon)
 {
 	random_device rd;
 	mt19937 rng;
@@ -296,7 +298,7 @@ void RunMany(int n, int algorithm, int size, double epsilon)
 	{
 		printf("\r%d/%d", i + 1, n);
 		fflush(stdout);
-		auto nit = Run(algorithm, size, epsilon, rng);
+		auto nit = Run(algorithm, delay, size, epsilon, rng);
 		min = std::min(min, nit);
 		max = std::max(max, nit);
 		sum += nit;
@@ -314,9 +316,11 @@ int main(int argc, char *argv[])
 	CommandLine::Integer nruns("n", false, "Number of times to run", 1, 100000, 1);
 	CommandLine::Boolean all("all", false, "Run all algorithms (used together with -n)");
 	CommandLine::Boolean dump("dump", false, "Print payoffs and strategies");
+	CommandLine::Integer delay("delay", false, "Averaging delay in iterations", 0, 100000, 0);
 	CommandLine::Parser::Parse(argc, argv);
 
 	if (!all) printf("Algorithm: %s\n", algorithmNames[algorithm]);
+	printf("Averaging delay (CFR+): %d\n", (int)delay);
 	printf("Matrix size: %dx%d\n", (int)size, (int)size);
 	printf("Epsilon: %f\n", (double)epsilon);
 	printf("N: %d\n", (int)nruns);
@@ -326,11 +330,11 @@ int main(int argc, char *argv[])
 		if (all)
 		{
 			for (int alg = 0; alg < AlgorithmCount; alg++)
-				RunMany(nruns, alg, size, epsilon);
+				RunMany(nruns, alg, delay, size, epsilon);
 		}
 		else
 		{
-			RunMany(nruns, algorithm, size, epsilon);
+			RunMany(nruns, algorithm, delay, size, epsilon);
 		}
 			
 		return 0;
@@ -352,7 +356,7 @@ int main(int argc, char *argv[])
 
 	do
 	{
-		m.Iteration(algorithm);
+		m.Iteration(algorithm, delay);
 
 		e = m.GetExploitability();
 
