1bd61b99555e8e67759117136c3b5df6abf5f74b
skario <ot@iki.fi>
2013-09-17 01:32:02 +0300

    -distribution (more random number distributions)

--- CFRPlusMatrix.cpp
+++ CFRPlusMatrix.cpp
@@ -11,35 +11,102 @@ using namespace std::chrono;
 const int AlgorithmCount = 3;
 char const *algorithmNames[] = { "Fictitious play", "CFR", "CFR+" };
 char const *wmodeNames[] = { "constant", "linear", "quadratic" };
-
+char const *distributionNames[] = { "uniform", "normal", "cauchy" };
 
 class MatrixGame
 {
 public:
-	MatrixGame(int size, mt19937 &rng)
+	struct Parameters
 	{
-		this->size = size;
-		iterationCount = 0;
-		payoffs.resize(size * size);
+		int size;
+		int distribution;
+		int algorithm;
+		int delay;
+		int wmode;
+	};
 
-		for (int p = 0; p < 2; p++)
+	MatrixGame(Parameters const &p, mt19937 &rng)
+		: p(p)
+	{
+		iterationCount = 0;
+		payoffs.resize(p.size * p.size);
+
+		for (int player = 0; player < 2; player++)
 		{
-			strategy[p].resize(size);
-			cfr[p].resize(size);
+			strategy[player].resize(p.size);
+			cfr[player].resize(p.size);
 		}
 
 		CreateRandom(rng);
 	}
 
+	int GetIterationCount() const { return iterationCount; }
+	double GetExploitability() const { return (BestResponse(0) + BestResponse(1)) / 2; }
+
+	void Iteration()
+	{
+		iterationCount++;
+
+		switch (p.algorithm)
+		{
+		case 0: FictitiousPlay(); break;
+		case 1: CFR(); break;
+		default: CFRPlus(); break;
+		}
+	}
+
+	void Dump()
+	{
+		printf("Payoffs:\n");
+
+		for (int a = 0; a < p.size; a++)
+		{
+			for (int b = 0; b < p.size; b++)
+			{
+				printf("%5.2f ", payoffs[a * p.size + b]);
+			}
+			printf("\n");
+		}
+
+		for (int player = 0; player < 2; player++)
+		{
+			auto ns = GetNormalizedStrategy(player);
+
+			printf(player == 0 ? "Row strategy:    " : "Column strategy: ");
+			
+			for (int i = 0; i < p.size; i++)
+				printf("%4.2f ", ns[i]);
+
+			printf("\n");
+		}
+	}
+
+private:
 	void CreateRandom(mt19937 &rng)
 	{
-		uniform_real_distribution<double> double_rnd(-1.0, 1.0);
-
-		for (int a = 0; a < size; a++)
+		switch(p.distribution)
 		{
-			for (int b = 0; b < size; b++)
+		case 0:
+			CreateRandom(rng, uniform_real_distribution<double>(-1.0, 1.0));
+			break;
+		case 1:
+			CreateRandom(rng, normal_distribution<double>(0, 0.5));
+			break;
+		default:
+			CreateRandom(rng, cauchy_distribution<double>(0, 0.02));
+			break;
+		}
+
+	}
+
+	template <class T>
+	void CreateRandom(mt19937 &rng, T distribution)
+	{
+		for (int a = 0; a < p.size; a++)
+		{
+			for (int b = 0; b < p.size; b++)
 			{
-				payoffs[a * size + b] = double_rnd(rng);
+				payoffs[a * p.size + b] = distribution(rng);
 			}
 		}
 
@@ -51,92 +118,31 @@ public:
 		*/
 	}
 
-	int GetIterationCount() const { return iterationCount; }
-	double GetExploitability() const { return (BestResponse(0) + BestResponse(1)) / 2; }
-
-	void FictitiousPlay()
-	{
-		iterationCount++;
-		FictitiousPlay(0);
-		FictitiousPlay(1);
-	}
-
-	void CFR()
-	{
-		iterationCount++;
-		CFR(0);
-		CFR(1);
-	}
-
-	void CFRPlus(int delay, int wmode)
-	{
-		iterationCount++;
-		CFRPlus(0, delay, wmode);
-		CFRPlus(1, delay, wmode);
-	}
-
-	void Iteration(int algorithm, int delay, int wmode)
-	{
-		switch (algorithm)
-		{
-		case 0: FictitiousPlay(); break;
-		case 1: CFR(); break;
-		default: CFRPlus(delay, wmode); break;
-		}
-	}
-
-	void Dump()
-	{
-		printf("Payoffs:\n");
-
-		for (int a = 0; a < size; a++)
-		{
-			for (int b = 0; b < size; b++)
-			{
-				printf("%5.2f ", payoffs[a * size + b]);
-			}
-			printf("\n");
-		}
-
-		for (int p = 0; p < 2; p++)
-		{
-			auto ns = GetNormalizedStrategy(p);
-
-			printf(p == 0 ? "Row strategy:    " : "Column strategy: ");
-			
-			for (int i = 0; i < size; i++)
-				printf("%4.2f ", ns[i]);
-
-			printf("\n");
-		}
-	}
-
-private:
-	double GetPayoff(int p, int a, int b) const
+	double GetPayoff(int player, int a, int b) const
 	{ 
-		if (p == 0)
-			return payoffs[a * size + b]; 
+		if (player == 0)
+			return payoffs[a * p.size + b]; 
 		else
-			return -payoffs[b * size + a]; 
+			return -payoffs[b * p.size + a]; 
 	}
 
 	vector<double> GetNormalizedStrategy(int player) const
 	{
-		vector<double> ns(size);
+		vector<double> ns(p.size);
 		double sum = 0;
 
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < p.size; i++)
 			sum += strategy[player][i];
 
 		if (sum > 0)
 		{
-			for (int i = 0; i < size; i++)
+			for (int i = 0; i < p.size; i++)
 				ns[i] = strategy[player][i] / sum;
 		}
 		else
 		{
-			for (int i = 0; i < size; i++)
-				ns[i] = 1.0 / size;
+			for (int i = 0; i < p.size; i++)
+				ns[i] = 1.0 / p.size;
 		}
 
 		return ns;
@@ -144,21 +150,21 @@ private:
 
 	vector<double> GetCurrentStrategy(int player) const
 	{
-		vector<double> cs(size);
+		vector<double> cs(p.size);
 		double sum = 0;
 
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < p.size; i++)
 			sum += max(0.0, cfr[player][i]);
 
 		if (sum > 0)
 		{
-			for (int i = 0; i < size; i++)
+			for (int i = 0; i < p.size; i++)
 				cs[i] = cfr[player][i] > 0 ? cfr[player][i] / sum : 0.0;
 		}
 		else
 		{
-			for (int i = 0; i < size; i++)
-				cs[i] = 1.0 / size;
+			for (int i = 0; i < p.size; i++)
+				cs[i] = 1.0 / p.size;
 		}
 
 		return cs;
@@ -170,11 +176,11 @@ private:
 
 		auto ns = GetNormalizedStrategy(player ^ 1);
 
-		for (int a = 0; a < size; a++)
+		for (int a = 0; a < p.size; a++)
 		{
 			double sum = 0;
 
-			for (int b = 0; b < size; b++)
+			for (int b = 0; b < p.size; b++)
 				sum += ns[b] * GetPayoff(player, a, b);
 
 			if (sum > maxSum)
@@ -184,6 +190,12 @@ private:
 		return maxSum;
 	}
 
+	void FictitiousPlay()
+	{
+		FictitiousPlay(0);
+		FictitiousPlay(1);
+	}
+
 	void FictitiousPlay(int player)
 	{
 		double maxSum = -numeric_limits<double>::max();
@@ -191,11 +203,11 @@ private:
 
 		auto ns = GetNormalizedStrategy(player ^ 1);
 
-		for (int a = 0; a < size; a++)
+		for (int a = 0; a < p.size; a++)
 		{
 			double sum = 0;
 
-			for (int b = 0; b < size; b++)
+			for (int b = 0; b < p.size; b++)
 				sum += ns[b] * GetPayoff(player, a, b);
 
 			if (sum > maxSum)
@@ -209,70 +221,91 @@ private:
 
 	}
 
+	void CFR()
+	{
+		CFR(0);
+		CFR(1);
+	}
+
 	void CFR(int player)
 	{
-		vector<double> cfu(size);
+		vector<double> cfu(p.size);
 		double ev = 0;
 
 		auto sp = GetCurrentStrategy(player);
 		auto so = GetCurrentStrategy(player ^ 1);
 
-		for (int a = 0; a < size; a++)
+		for (int a = 0; a < p.size; a++)
 		{
 			cfu[a] = 0;
 
-			for (int b = 0; b < size; b++)
+			for (int b = 0; b < p.size; b++)
 				cfu[a] += so[b] * GetPayoff(player, a, b);
 
 			ev += sp[a] * cfu[a];
 		}
 
-		for (int a = 0; a < size; a++)
+		for (int a = 0; a < p.size; a++)
 			cfr[player][a] += cfu[a] - ev;
 
-		for (int a = 0; a < size; a++)
+		for (int a = 0; a < p.size; a++)
 			strategy[player][a] += sp[a];
 
 	}
 
-	void CFRPlus(int player, int delay, int wmode)
+	void CFRPlus()
 	{
-		vector<double> cfu(size);
+		CFRPlus(0);
+		CFRPlus(1);
+	}
+
+	void CFRPlus(int player)
+	{
+		vector<double> cfu(p.size);
 		double ev = 0;
 
 		auto sp = GetCurrentStrategy(player);
 		auto so = GetCurrentStrategy(player ^ 1);
 
-		for (int a = 0; a < size; a++)
+		for (int a = 0; a < p.size; a++)
 		{
 			cfu[a] = 0;
 
-			for (int b = 0; b < size; b++)
+			for (int b = 0; b < p.size; b++)
 				cfu[a] += so[b] * GetPayoff(player, a, b);
 
 			ev += sp[a] * cfu[a];
 		}
 
-		for (int a = 0; a < size; a++)
+		for (int a = 0; a < p.size; a++)
 			cfr[player][a] = std::max(0.0, cfr[player][a] + cfu[a] - ev);
 
 		double w;
 
-		switch(wmode)
+		if (iterationCount > p.delay)
 		{
-		case 0: w = iterationCount > delay ? 1 : 0; break;
-		case 1: w = iterationCount > delay ? (iterationCount - delay) : 0; break;
-		default: w = iterationCount > delay ? (iterationCount - delay) * (iterationCount - delay) : 0; break;
+			int t = iterationCount - p.delay;
+
+			switch(p.wmode)
+			{
+			case 0: w = 1; break;
+			case 1: w = t; break;
+			default: w = t * t; break;
+			}
+		}
+		else
+		{
+			w = 0;
 		}
 
-		for (int a = 0; a < size; a++)
+		for (int a = 0; a < p.size; a++)
 			strategy[player][a] += sp[a] * w;
 
 	}
 
 
 private:
-	int size;
+	Parameters p;
 	int iterationCount;
 	vector<double> payoffs;
 	vector<double> strategy[2];
@@ -280,25 +313,21 @@ private:
 
 };
 
-int Run(int algorithm, int delay, int wmode, int size, double epsilon, mt19937 &rng)
+int Run(MatrixGame::Parameters const &p, double epsilon, mt19937 &rng)
 {
-	MatrixGame m(size, rng);
+	MatrixGame m(p, rng);
 	double e;
 	do
 	{
-		m.Iteration(algorithm, delay, wmode);
+		m.Iteration();
 		e = m.GetExploitability();
 	} while (e > epsilon);
 
 	return m.GetIterationCount();
 }
 
-void RunMany(int n, int algorithm, int delay, int wmode, int size, double epsilon)
+void RunMany(MatrixGame::Parameters const &p, int n, double epsilon, mt19937 &rng)
 {
-	random_device rd;
-	mt19937 rng;
-	rng.seed(rd());
-
 	double sum = 0;
 	int min = numeric_limits<int>::max();
 	int max = numeric_limits<int>::min();
@@ -307,13 +336,13 @@ void RunMany(int n, int algorithm, int delay, int wmode, int size, double epsilo
 	{
 		printf("\r%d/%d", i + 1, n);
 		fflush(stdout);
-		auto nit = Run(algorithm, delay, wmode, size, epsilon, rng);
+		auto nit = Run(p, epsilon, rng);
 		min = std::min(min, nit);
 		max = std::max(max, nit);
 		sum += nit;
 	}
 
-	printf("\r%-16s | min %-4d | max %-6d | avg %.1f\n", algorithmNames[algorithm], min, max, sum / n);
+	printf("\r%-16s | min %-4d | max %-6d | avg %.1f\n", algorithmNames[p.algorithm], min, max, sum / n);
 
 }
 
@@ -321,6 +350,7 @@ int main(int argc, char *argv[])
 {
 	CommandLine::Integer algorithm("a", false, "Algorithm (0 = Fictitious play, 1 = CFR, 2 = CFR+)", 0, 2, 2);
 	CommandLine::Integer size("s", false, "Matrix size", 2, 100000, 1000);
+	CommandLine::Integer distribution("distribution", false, "Random number distribution (0 = uniform, 1 = normal, 2 = cauchy)", 0, 2, 0);
 	CommandLine::Real epsilon("e", false, "Epsilon", 0.000000000001, 1, 0.0001);
 	CommandLine::Integer nruns("n", false, "Number of times to run", 1, 100000, 1);
 	CommandLine::Boolean all("all", false, "Run all algorithms (used together with -n)");
@@ -329,23 +359,35 @@ int main(int argc, char *argv[])
 	CommandLine::Integer wmode("w", false, "Weighting mode (0 = constant, 1 = linear, 2 = quadratic)", 0, 2, 1);
 	CommandLine::Parser::Parse(argc, argv);
 
+	MatrixGame::Parameters p;
+	p.size = size;
+	p.distribution = distribution;
+	p.algorithm = algorithm;
+	p.delay = delay;
+	p.wmode = wmode;
+
 	if (!all) printf("Algorithm: %s\n", algorithmNames[algorithm]);
 	printf("Averaging delay (CFR+): %d\n", (int)delay);
 	printf("Weighting mode (CFR+): %s\n", wmodeNames[(int)wmode]);
 	printf("Matrix size: %dx%d\n", (int)size, (int)size);
+	printf("Random number distribution: %s\n", distributionNames[distribution]);
 	printf("Epsilon: %f\n", (double)epsilon);
 	printf("N: %d\n", (int)nruns);
+
+	random_device rd;
+	mt19937 rng;
+	rng.seed(rd());
 
 	if (nruns > 1)
 	{
 		if (all)
 		{
-			for (int alg = 0; alg < AlgorithmCount; alg++)
-				RunMany(nruns, alg, delay, wmode, size, epsilon);
+			for (p.algorithm = 0; p.algorithm < AlgorithmCount; p.algorithm++)
+				RunMany(p, nruns, epsilon, rng);
 		}
 		else
 		{
-			RunMany(nruns, algorithm, delay, wmode, size, epsilon);
+			RunMany(p, nruns, epsilon, rng);
 		}
 			
 		return 0;
@@ -353,11 +395,7 @@ int main(int argc, char *argv[])
 
 	printf("init\n");
 
-	random_device rd;
-	mt19937 rng;
-	rng.seed(rd());
-
-	MatrixGame m(size, rng);
+	MatrixGame m(p, rng);
 	double e;
 
 	printf("start\n");
@@ -367,13 +405,11 @@ int main(int argc, char *argv[])
 
 	do
 	{
-		m.Iteration(algorithm, delay, wmode);
-
+		m.Iteration();
 		e = m.GetExploitability();
-
 		auto t = duration_cast<milliseconds>(clock.now() - startTime).count();
-
 		printf("i=%d t=%.2f e=%.6f\n", m.GetIterationCount(), t / 1000.0, e);
+
 	} while (e > epsilon);
 
 	if (dump) m.Dump();
